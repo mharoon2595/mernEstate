587f0eaa359f0c7deb2c185a59541b3fdcaf9c65
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-08 08:35:37 +0530

    socket final

--- src/utils/SocketContext.jsx
+++ src/utils/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   const { userId } = useContext(UserContext);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://mernsocket.onrender.com"));
   }, []);
 
   useEffect(() => {
