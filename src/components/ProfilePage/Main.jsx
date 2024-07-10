bbcb06587801d29fb07352010399a9c4968ebd1d
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-10 09:18:23 +0530

    _

--- src/components/ProfilePage/Main.jsx
+++ src/components/ProfilePage/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
   return (
     <>
       <div className="hidden md:flex absolute w-2/5 h-full bg-[#FFDAB9] z-[-1] right-0 top-0"></div>
-      <div className="flex flex-col h-full md:flex md:flex-row  md:min-h-[calc(100vh-120px)]">
+      <div className="flex flex-col min-h-full md:flex md:flex-row  md:min-h-[calc(100vh-120px)]">
         <div className="block md:flex md:flex-col md:gap-5 md:w-3/5 md:min-h-full">
           {updateFlag ? (
             <UpdateProfile />
