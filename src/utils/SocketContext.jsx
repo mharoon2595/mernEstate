0958920b46c534e877b8b2c0050578374626d9e6
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-15 16:00:19 +0530

    resolving issues with socket

--- src/utils/SocketContext.jsx
+++ src/utils/SocketContext.jsx
@@ -11,14 +11,12 @@ import { UserContext } from "./Context";
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
-  const [socket, setSocket] = useState(null);
-  const { userId, loggedIn } = useContext(UserContext);
+  // const [socket, setSocket] = useState(null);
+  const { userId } = useContext(UserContext);
 
-  useEffect(() => {
-    setSocket(
-      io("https://current-dominica-devinthemaking-721da948.koyeb.app/")
-    );
-  }, [userId]);
+  const socket = io(
+    "https://current-dominica-devinthemaking-721da948.koyeb.app/"
+  );
 
   useEffect(() => {
     userId && socket?.emit("newUser", userId);
